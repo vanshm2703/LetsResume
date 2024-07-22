29cc1e5ba4bf6ad763081d9563b6a39248b1387f
vanshm2703 <128774223+vanshm2703@users.noreply.github.com>
2024-07-22 23:14:55 +0530

    comple

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,1 +1,1 @@
-export const url = "https://06c9025e-6f17-452e-9a86-112ab0c650a7-00-yrcqkh4jxzfb.sisko.replit.dev:3000"; 
+// export const url = "https://06c9025e-6f17-452e-9a86-112ab0c650a7-00-yrcqkh4jxzfb.sisko.replit.dev:3000"; 
